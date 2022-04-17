cd7b5a83668d0fda842d821f58c9c8feccd7628f
Suganth S <33140295+Suganth-S@users.noreply.github.com>
2022-04-17 23:14:28 +0530

    Add files via upload

---
+++ Leetcode/Easy/BitManupilation/ReverseBits.java
@@ -0,0 +1,17 @@
+package Leetcode.Easy.BitManupilation;
+
+import java.util.Arrays;
+
+public class ReverseBits {
+
+	public static void main(String[] args) {
+		int num = 00000010100101000001111010011100;
+		num = ((num & 0xffff0000) >>> 16) | ((num & 0x0000ffff) << 16);
+        num = ((num & 0xff00ff00) >>> 8) | ((num & 0x00ff00ff) << 8);
+        num = ((num & 0xf0f0f0f0) >>> 4) | ((num & 0x0f0f0f0f) << 4);
+        num = ((num & 0xcccccccc) >>> 2) | ((num & 0x33333333) << 2);
+        num = ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1);
+		System.out.println(num);
+	}
+
+}
